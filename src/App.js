ada883c3ca060c0ace2b7a6433bea7d12d19971d
Nicholas Brandão <nicholas.computacao@outlook.com>
2021-06-10 23:57:34 +0100

    Added Add and Delete items in the list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { List, Paper, ListItem, ListItemText, IconButton, Checkbox, TextField  } from "@material-ui/core";
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import AddIcon from '@material-ui/icons/Add';
+import styles from './styles';
+import { withStyles } from "@material-ui/styles";
 
-function App() {
+const App = ({classes}) => {
+
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  const changeItem = (newItemValue, index) => {
+    const updatedItems = items.map((item, i) => {
+      if(i === index){
+        return newItemValue;
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  };
+
+  const excludeItem = (index) => {
+    const filteredItems = items.filter((_, i) => i !== index);
+    setItems(filteredItems);
+  }
+
+  const addItem = () => {
+    if(!newItem) return;
+    setItems([...items, {todo: newItem, isCompleted: false}]);
+    setNewItem('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Paper className={classes.paper}>
+        <List>
+          {
+            items.map((item, index) => (
+              <ListItem key={index}>
+                <Checkbox 
+                  color='primary'
+                  value={item.isCompleted}
+                  onClick={() => changeItem({...item, isCompleted: !item.isCompleted}, index)}
+                />
+                <ListItemText primary={item.todo} />
+                <IconButton onClick={() => excludeItem(index)}>
+                  <DeleteForeverIcon />
+                </IconButton>
+              </ListItem>
+            ))
+          }
+
+          <ListItem>
+            <TextField 
+              value={newItem} 
+              onChange={({target}) => setNewItem(target.value)} 
+              fullWidth
+            />
+            <IconButton>
+              <AddIcon onClick={addItem} />
+            </IconButton>
+          </ListItem>
+        </List>
+      </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+    paper: {
+        width: '50%', 
+        position: 'absolute', 
+        left: '50%', 
+        top: '50%', 
+        transform: 'translate(-50%, -50%)'
+    }
+}
+
+export default styles;
